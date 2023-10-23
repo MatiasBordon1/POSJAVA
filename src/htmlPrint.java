104788bee5d04bf56d9d798a223015694c65478c
Matias Bordon <matzaia2001@gmail.com>
2023-10-23 19:29:51 -0300

    print polenta 2

--- src/htmlPrint.java
+++ src/htmlPrint.java
@@ -18,16 +18,21 @@ public class htmlPrint {
         int columnCount = model.getColumnCount();
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.write("<html><head><title>Table Data</title></head><body><table border='1'>");
+            writer.write("<html><head><title>Table Data</title>");
+            writer.write("<style>");
+            writer.write("body { font-family: Arial, sans-serif; margin: 10px; }");
+            writer.write("table { width: 100%; border-collapse: collapse; }");
+            writer.write("th, td { border: 1px solid #333; padding: 5px; text-align: left; }");
+            writer.write("tr:nth-child(even) { background-color: #f2f2f2; }");
+            writer.write("</style></head><body>");
+            writer.write("<table border='1'>");
 
-           
             writer.write("<tr>");
             for (int i = 0; i < columnCount; i++) {
                 writer.write("<th>" + model.getColumnName(i) + "</th>");
             }
             writer.write("</tr>");
 
-           
             for (int row = 0; row < rowCount; row++) {
                 writer.write("<tr>");
                 for (int column = 0; column < columnCount; column++) {
